36509da61f9378bcf8ffd838f1e3beeb5872f08f
Nelson Silva <nelsonsilva200017@gmail.com>
2026-09-10 15:03:44 +0200

    Final version

---
+++ Server/InMemoryRepositories/CommentInMemoryRepository.cs
@@ -0,0 +1,67 @@
+﻿using Entities;
+using RepositoryContracts;
+
+namespace InMemoryRepositories;
+
+public class CommentInMemoryRepository : ICommentRepository
+{
+    private List<Comment> comments = new();
+
+    public CommentInMemoryRepository()
+    {
+        comments.Add(new Comment{Body="I dont like this post",Id = 1,UserId = 1});
+        comments.Add(new Comment{Body="I like this post",Id = 2,UserId = 2});
+    }
+    
+    public Task<Comment> AddAsync(Comment comment)
+    {
+        comment.Id = comments.Any()
+            ? comments.Max(p => (int)p.Id) + 1 : 1;
+        comments.Add(comment);
+        return Task.FromResult(comment);
+    }
+    
+    public Task UpdateAsync(Comment comment)
+    {
+        Comment? existingComment = comments.SingleOrDefault(p => p.Id == comment.Id);
+        if (existingComment is null)
+        {
+            throw new InvalidOperationException(
+                $"Comment with ID '{comment.Id}' not found");
+        }
+
+        comments.Remove(existingComment);
+        comments.Add(comment);
+
+        return Task.CompletedTask;
+    }
+    
+    public Task DeleteAsync(int id)
+    {
+        Comment? commentToRemove = comments.SingleOrDefault(p => p.Id == id);
+        if (commentToRemove is null)
+        {
+            throw new InvalidOperationException(
+                $"Comment with ID '{id}' not found");
+        }
+
+        comments.Remove(commentToRemove);
+        return Task.CompletedTask;
+    }
+    
+    public Task<Comment> GetSingleAsync(int id)
+    {
+        Comment? comment = comments.SingleOrDefault(p => p.Id == id);
+        if (comment is null)
+        {
+            throw new InvalidOperationException(
+                $"Comment with ID '{id}' not found");
+        }
+        return Task.FromResult(comment);
+    }
+    
+    public IQueryable<Comment> GetMany()
+    {
+        return comments.AsQueryable();
+    }
+}
---
+++ Server/InMemoryRepositories/UserInMemoryRepository.cs
@@ -0,0 +1,71 @@
+﻿using Entities;
+using RepositoryContracts;
+namespace InMemoryRepositories;
+
+public class UserInMemoryRepository : IUserRepository
+{
+    private List<User> users = new();
+
+    public UserInMemoryRepository()
+    {
+        AddDummyData();
+    }
+
+    private void AddDummyData()
+    {
+        users.Add(new User {Id = 1,Password = "12345",Username = "User1"});
+        users.Add(new User {Id = 2,Password = "12345",Username = "User2"});
+    }
+    
+    public Task<User> AddAsync(User user)
+    {
+        user.Id = users.Any()
+            ? users.Max(p => (int)p.Id) + 1 : 1;
+        users.Add(user);
+        return Task.FromResult(user);
+    }
+    
+    public Task UpdateAsync(User user)
+    {
+        User? existinguser = users.SingleOrDefault(p => p.Id == user.Id);
+        if (existinguser is null)
+        {
+            throw new InvalidOperationException(
+                $"user with ID '{user.Id}' not found");
+        }
+
+        users.Remove(existinguser);
+        users.Add(user);
+
+        return Task.CompletedTask;
+    }
+    
+    public Task DeleteAsync(int id)
+    {
+        User? userToRemove = users.SingleOrDefault(p => p.Id == id);
+        if (userToRemove is null)
+        {
+            throw new InvalidOperationException(
+                $"user with ID '{id}' not found");
+        }
+
+        users.Remove(userToRemove);
+        return Task.CompletedTask;
+    }
+    
+    public Task<User> GetSingleAsync(int id)
+    {
+        User? user = users.SingleOrDefault(p => p.Id == id);
+        if (user is null)
+        {
+            throw new InvalidOperationException(
+                $"user with ID '{id}' not found");
+        }
+        return Task.FromResult(user);
+    }
+    
+    public IQueryable<User> GetMany()
+    {
+        return users.AsQueryable();
+    }
+}
